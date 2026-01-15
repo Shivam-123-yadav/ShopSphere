fdd0aa1019584d285e207f6ae0c40b9cc07e3952
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2026-01-15 14:37:08 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@0dbq82r@*2j7^nk_yio(xva5a28tknih*38v8zkdfs#pnv50-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 AUTH_USER_MODEL = "shop.User"
 
