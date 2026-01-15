eb475f7e2e6e95b78727bae301eb4b0d10da99d5
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2026-01-15 14:47:36 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,6 +153,9 @@ TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "templates")]
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
