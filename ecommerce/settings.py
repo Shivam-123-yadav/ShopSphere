caa14f071965ca317eec0ddc0c4af80f8307c387
Shivam-123-yadav <141988688+Shivam-123-yadav@users.noreply.github.com>
2026-01-15 17:06:14 +0530

    Add files via upload

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -153,9 +152,6 @@ TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "templates")]
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
